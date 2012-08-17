8c024c26122409a44fe57decfff49ea15d940cd9
Jason Parrott <jason.parrott@gree.co.jp>
2012-08-17 17:20:07 +0900

    Updated to new build system. (JSWorkBench)

---
+++ src/core/StageManager.js
@@ -0,0 +1,22 @@
+/**
+ * @author Jason Parrott
+ * 
+ * Copyright (C) 2012 Jason Parrott.
+ * This code is licensed under the zlib license. See LICENSE for details.
+ */
+
+(function(global) {
+
+  var theatre = global.theatre;
+
+  /**
+   * The overall manager of all Actors on a Stage.
+   * @name theatre.StageManager
+   * @constructor
+   * @extends theatre.Actor
+   */
+  var StageManager = theatre.createActor('StageManager');
+
+  theatre.define('theatre.StageManager', StageManager);
+
+}(this));
